3770820449fed1936c7cfb7e69a3582f34b367d2
Matheus Marques <prizma.desenvolvimento@gmail.com>
2020-10-28 13:34:25 -0300

    prospecção criada

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -76,6 +76,17 @@ class BiddingsController < ApplicationController
     end
   end
 
+  def prospection
+  @q = Bidding.ransack(params[:q])
+
+  @biddings = @q.result
+          .order(date: :asc)
+
+  respond_to do |format|
+    format.html
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_bidding
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  get 'prospection', to: 'biddings#prospection'
   get 'biddings', to: 'biddings#reports'
   get 'certificates', to: 'certificates#reports'
  
   resources :certificates
   resources :biddings
+
   get 'home/index'
   root 'biddings#reports'
-  root 'certificates#reports'
-  
   
 end
