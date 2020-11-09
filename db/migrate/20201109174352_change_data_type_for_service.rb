c8afb1b4ac2c797875de7d499e9c892d213729ed
Alan Barile <alan.barile@gmail.com>
2020-11-09 15:23:39 -0300

    Alteração ambiente Atestados

--- db/migrate/20201109174352_change_data_type_for_service.rb
+++ db/migrate/20201109174352_change_data_type_for_service.rb
@@ -1,6 +1,6 @@
 class ChangeDataTypeForService < ActiveRecord::Migration[5.2]
   def change
-     change_column(:certificates, :kind_of_service, :integer)
+     change_column(:certificates, :kind_of_service, 'integer USING CAST(column_name AS integer)')
 	 rename_column(:certificates, :kind_of_service, :kind_of_service_id)
   end
   
