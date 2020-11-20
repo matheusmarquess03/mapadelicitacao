fcd2b87e77b1b568d46f55a69ae290bfbf58ccf0
Alan Barile <alan.barile@gmail.com>
2020-11-20 06:44:56 -0300

    Correcao problema UTF-8

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require_relative 'application'
 
 #Encoding.default_internal = Encoding::UTF_8
-Encoding.default_external = 'UTF_8'
+Encoding.default_external = Encoding::UTF_8
 
 # Initialize the Rails application.
 Rails.application.initialize!
