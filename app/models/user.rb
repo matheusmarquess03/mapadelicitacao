631fafb888898d46bde786c2cf7dd0c742901b67
Matheus Marques <prizma.desenvolvimento@gmail.com>
2020-12-09 10:42:33 -0300

    correção devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable, timeout_in: 2.minutes 
+         :recoverable, :rememberable, :validatable, :timeoutable, timeout_in: 30.minutes 
 end
